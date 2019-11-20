cb2361a433ef4e46d5bb70f273ceb0c10d519f82
DanielMenicucciTeixeira <36136914+DanielMenicucciTeixeira@users.noreply.github.com>
2019-11-20 14:12:19 -0500

    2019-11-20_1411

--- ComponentFramework1.0.4/ComponentFramework/simpleReflectionVert.glsl
+++ ComponentFramework1.0.4/ComponentFramework/simpleReflectionVert.glsl
@@ -8,6 +8,7 @@ out vec3 Direction;
 //out vec3 ReflectionVector;
 out vec3 Position;
 out vec3 CameraPosition;
+out float AngleOfInsidenseCos;
 
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
@@ -18,12 +19,14 @@ uniform float time;
 
 void main()
 {
-	float distortionLevel = 0.2;
-	float distortionSpeed = 1.0;
+	float distortionLevel = 0.0;
+	float distortionSpeed = 5.0;
 	CameraPosition = vec3(cameraPosition[3][0],cameraPosition[3][1],cameraPosition[3][2]);
+	
+	AngleOfInsidenseCos = dot(normalize(CameraPosition), normalize(normal.xyz));
 	Normal = mat3(transpose(inverse(modelMatrix))) * normal.xyz;
 	Position = vec3(position + (distortionLevel * cos(time) * normalize(normal)));
 	//Position = vec3(modelMatrix * position);
 	
-	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position + (distortionLevel * cos(time) * normalize(normal)));
+	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position + (distortionLevel * cos(time * distortionSpeed) * normalize(normal)));
 }
--- ComponentFramework1.0.4/ComponentFramework/simpleRefractionFrag.glsl
+++ ComponentFramework1.0.4/ComponentFramework/simpleRefractionFrag.glsl
@@ -7,13 +7,14 @@ in vec3 Direction;
 //in vec3 ReflectionVector;
 in vec3 Position;
 in vec3 CameraPosition;
+in float AngleOfInsidenseCos;
 
 uniform samplerCube enviroMap;
 
 void main()
 {
 	float refractionRatio = 1.0/1.309;
-	float reflectionRation = 0.3;
+	float reflectionRation = AngleOfInsidenseCos;
 
 	vec3 viewVector = normalize(Position + CameraPosition);
 
@@ -22,6 +23,6 @@ void main()
 	vec4 RefractionColor = texture(enviroMap, RefractionVector);
 	vec4 ReflectionColor = texture(enviroMap, ReflectionVector);
 	FragmentColor = mix(RefractionColor, ReflectionColor, reflectionRation);
-	FragmentColor = mix(FragmentColor, vec4(0.2f, 0.2f, 1.0f, 0.0f), 0.3f);
+	FragmentColor = mix(FragmentColor, vec4(0.2f, 0.2f, 1.0f, 0.0f), 0.2f);
 	//FragmentColor = vec4 (1.0f, 0.0f, 0.0f, 0.0f);FragmentColor = texture(enviroMap, Normal);
 }
